f48fddbaa063fcf5f54870f2e4686dfd9fb026d2
JongIn Yoon <bellpp93@gmail.com>
2022-01-21 17:45:04 +0900

    한글오류test

--- src/샘플2방식/MessageBeanEn.java
+++ src/샘플2방식/MessageBeanEn.java
@@ -1,7 +1,7 @@
 package 샘플2방식;
 //					클래스이름 implements 인터페이스이름
 public class MessageBeanEn implements MessageBean {
-	
+	//영어
 	@Override  //"약속"의 의미 => ~해야한다.
 	public void sayHello(String name) {
 		System.out.println("Hello, " + name + "!");
--- src/샘플2방식/MessageBeanKr.java
+++ src/샘플2방식/MessageBeanKr.java
@@ -1,7 +1,7 @@
 package »ùÇÃ2¹æ½Ä;
 
 public class MessageBeanKr implements MessageBean {
-
+	//ÇÑ±Û
 	@Override
 	public void sayHello(String name) {
 		System.out.println("¾È³çÇÏ¼¼¿ä, " + name + "´Ô");
